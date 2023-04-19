64e9b00243bf3546e58cdcdaea148558b518f23f
bcs1993 <you@example.com>
2023-04-18 22:53:18 -0500

    Starter Server File

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { config } from 'dotenv';
+
+const app = express();
+
+
+/** Middlewares */
+app.use(morgan('tiny'))
+app.use(cors());
+app.use(express.json());
+config();
+
+
+/** App Port */
+const port = process.env.PORT || 8080;
+
+
+
+/** Routes */
+app.get('/', (req, res) => {
+    try {
+        res.json("Get Request");
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server connected to http://localhost:${port}`);
+});
